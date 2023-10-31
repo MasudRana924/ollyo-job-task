b5049231c9f53dccf76c37bc118c5fca7453fbc9
MasudRana15924 <masudrana15924@outlook.com>
2023-10-31 19:32:15 +0600

    preloader added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Preloader from './loader/Preloader';
 function App() {
-
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  })
   return (
-    <div className="App">
+    <div>
+      {loading ? <div>
+        <Preloader></Preloader>
+      </div> : <div className="App">
 
+      </div>}
     </div>
   );
 }
